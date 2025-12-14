c5491985897eb9289ab31745b7890d2abf049598
Santiago <st281306@fi365.ort.edu.uy>
2025-12-14 09:12:36 -0300

    project name changes

--- Entrega-front-Reto1/src/app/app.spec.ts
+++ Entrega-front-Reto1/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, nacional-front');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Entrega-front-Reto1');
   });
 });
